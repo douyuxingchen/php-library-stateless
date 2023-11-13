09708ef6b7a6e2e56d4fc08b7f50dac455bc2fb5
libin <58850169@qq.com>
2023-11-13 12:51:04 +0800

    fix：tpl6模版短信参数链接添加 (#14)
    (cherry picked from commit 3d84b93cc9de8575433f03d282e01a693003bfaf)
    
    Co-authored-by: libin <libin@douyuxingchen.com>

--- tests/Unit/Sms/FeigeSmsTest.php
+++ tests/Unit/Sms/FeigeSmsTest.php
@@ -3,6 +3,7 @@ namespace Tests\Unit\Sms;
 
 use Douyuxingchen\PhpLibraryStateless\Sms\FeigeSmsProvider;
 use Douyuxingchen\PhpLibraryStateless\Sms\Params\FeigeParamsGen;
+use Douyuxingchen\PhpLibraryStateless\Sms\Params\ParamsBuilder;
 use Douyuxingchen\PhpLibraryStateless\Sms\SmsBuilder;
 use PHPUnit\Framework\TestCase;
 
@@ -100,6 +101,45 @@ class FeigeSmsTest extends TestCase
             ->setExtNo("666")
             ->setTemplateId($temp_code)
             ->setContent('我是测试链接')
+            ->setSendTime()
+            ->build();
+
+        $sms->setEnv([
+            'apikey' => lib_env('FEIGE_APIKEY'),
+            'secret' => lib_env('FEIGE_SECRET'),
+        ]);
+
+        $res = $sms->sendTemplate();
+
+        if(!$res->isStatus()) {
+            var_dump($res->getMessage());
+            var_dump($res->getData());
+        }
+
+        $this->assertEquals(true, $res->isStatus());
+    }
+
+    // 测试
+    // 模版短信 - 渠道推广活动通知加小云
+    // ./vendor/bin/phpunit --filter testActiveMsgAddYun2 ./tests/Unit/Sms/FeigeSmsTest.php
+    public function testActiveMsgAddYun2()
+    {
+        $smsTemplateKey = FeigeParamsGen::KEY_ACTIVE_MSG_ADD_YUN;
+        $params['link'] = 'https://www.yunkeyouxuan.com';
+
+        $content = (new ParamsBuilder())->setProvider(new FeigeParamsGen())
+                ->setParams([
+                    'link' => $params['link'] ?? null,
+                ])->setTempCode(FeigeParamsGen::KEY_CODE[$smsTemplateKey])
+                ->build()
+                ->genParams();
+
+        $sms = (new SmsBuilder())->setProvider(new FeigeSmsProvider())
+            ->setMobile('15711273395')
+            ->setSignId(185283)
+            ->setExtNo("666")
+            ->setTemplateId(FeigeParamsGen::KEY_CODE[$smsTemplateKey])
+            ->setContent($content)
             ->setSendTime()
             ->build();
 
